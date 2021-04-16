33cddc3b386bf4f6b88b00ed3176c9bdb93a0408
Nitesh Sharma <niteshsharma312000@gmail.com>
2021-04-16 15:59:36 +0530

    URL Encode removed

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.use(cors())
 // view engine setup
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
